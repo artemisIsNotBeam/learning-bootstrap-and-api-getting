edb28f58bdbadf6ca081370c98b6d5e33f556528
FPS man <66878297+artemisIsNotBeam@users.noreply.github.com>
2022-03-24 01:13:08 +0000

    returns the title and year

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
 $(
-	function(){
-		var seartchItem = "star Wars";
-		console.log("one");
-		$.get({
-			url:"http://www.omdbapi.com/?i=tt3896198&apikey=740986f0"
-		});
-	}
-)
+
+  function()
+  {
+    var searchTerm = "Star Wars";
+    //console.log("one");
+    $.get({
+
+      url: "https://www.omdbapi.com/?i=tt3896198&apikey=740986f0&s="+searchTerm,
+      success: function(data,textStatus,jqXHR)
+        {
+          console.log("hello");
+					let Search = data["Search"]
+					for (let i=0;i<Search.length;i++){
+						let Move = Search[i];
+						console.log(Move["Title"]);
+						console.log(Move["Year"]);
+					}
+        },
+      error: function()
+        {
+          alert("There was an error");
+        }
+
+      
+    }); // End of $.get
+  }//end of function
+  
+);//end of jQuery
