71ba7dbc495bd4d18301ac4427075f55bf6cbca3
FPS man <66878297+artemisIsNotBeam@users.noreply.github.com>
2022-04-14 01:10:39 +0000

    even more work

--- script.js
+++ script.js
@@ -13,9 +13,59 @@ $(
 					let Search = data["Search"];
 					let movieData = Search;
 					for (let x=0;x<Search.length;x++){
-						$("#list-view > table > tbody").append("<tr><td><p class='atitle'>"+movieData[x]["Title"]+"</p></td><td>"+movieData[x]["Year"]+"</td> <td><img width =125px height=200px src="+movieData[x]["Poster"]+"></td></tr>");
+
+						
+						$.get({
+							url:"https://www.omdbapi.com/?i=tt3896198&apikey=740986f0&i="+moveData[x]["imdb"],
+
+							success: function(moveiDetail,_textStatus,weirdParam	) {
+								
+							},
+							error: function(){
+								alert("promblem");
+							}	
+				
+
+						
+						});
+
+						$("#list-view > table > tbody").append("<tr><td><p class='atitle' height=220px>"+movieData[x]["Title"]+"</p></td><td>"+movieData[x]["Year"]+"</td> <td><img width =155px height=200px src="+movieData[x]["Poster"]+"></td></tr>");
+					console.log("hi nice code");
+					}
+        },
+      error: function()
+        {
+          alert("There was an error");
+        }
+
+      
+    }); // End of $.get
+  }//end of function
+  
+);//end of jQuery
+
+//jQuery for second one
+$(
+
+  function()
+  {
+    var searchTerm = "Star Wars";
+    //console.log("one");
+    $.get({
+
+      url: "https://www.omdbapi.com/?i=tt3896198&apikey=740986f0&t="+searchTerm,
+      success: function(data,textStatus,jqXHR)
+        {
+          console.log("hello");
+					let Search = data["Search"];
+					let movieData = Search;
+					for (let x=0;x<Search.length;x++){
+						$("#advanced-view > table > tbody").append("<tr><td><p class='atitle'>"+movieData[x]["Title"]["Value"]+"</p></td><td>"+movieData[x]+"</td> <td><img width =125px height=200px src="+movieData[x]["Poster"]+"></td><td>"+movieDatat["imdb"]+"</td></tr>");
+
+
 
 					}
+					console.log(movieData["Rating"]);
         },
       error: function()
         {
